1a4f099fa73405039f01498d812d88f276c4d8f4
Danny Solivan <dark@nightconcept.net>
2025-06-24 11:00:35 -0700

    fix: Fixed sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,10 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://example.com',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'Boba Lang',
 			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
